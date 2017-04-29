7d86b993de520ab62d763d8e3eb6031164ffb566
jainrahul1234 <jainrahul.cary@gmail.com>
2017-04-30 01:32:28 +0530

    Update chaincode_intermediate.go
    updated more functiions .. build failing

--- intermediate/chaincode_intermediate.go
+++ intermediate/chaincode_intermediate.go
@@ -16,22 +16,28 @@ limitations under the License.
 
 package main
 
+// Import all the necessary libraries
 import (
 "errors"
 "fmt"
 "encoding/json"
 "github.com/hyperledger/fabric/core/chaincode/shim"
+"strconv"
+//"github.com/hyperledger/fabric/protos/peer"
 )
 
-//custom data models
+//custom data models for Package Information
 type PackageInfo struct {
-//    PackageID string `json:"id"`
-  Shipper   string `json:"shipper"`
-  Consignee string `json:"consignee"`
-  Temprature string `json:"temprature"`
+  PkgId      string `json:"packageid"`
+  Shipper    string `json:"shipper"`
+  Insurer    string `json:"insurer"`
+  Consignee  string `json:"consignee"`
+  Owner      string `json:"owner"`
+  Temprature int `json:"temprature"`
   PackageDes string `json:"packagedes"`
-  Owner string `json:"owner"`
+  PkgStatus  string `json:"pkgstatus"`
 }
+
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
 }
@@ -40,35 +46,43 @@ func main() {
 err := shim.Start(new(SimpleChaincode))
 if err != nil {
   fmt.Printf("Error starting Simple chaincode: %s", err)
-}
+  }
 }
 
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-if len(args) != 5 {
-  return nil, errors.New("Incorrect number of arguments. Expecting 1")
-}
+if len(args) != 6 {
+  return nil, errors.New("Incorrect number of arguments. Expecting 6 in order of Shipper, Insurer, Consignee, Temprature, PackageDes, Owner ")
+  }
 
 var packageinfo PackageInfo
-//packageinfo = PackageInfo{"UPS","RAHUL",2,"ANTIBIOTICS"}
+var err error
 
-
+packageinfo.PkgId = "1Z20170426"
 packageinfo.Shipper = args[0]
-packageinfo.Consignee  = args[1]
-packageinfo.Temprature = args[2]
-packageinfo.PackageDes = args[3]
-packageinfo.Owner = args[4]
+packageinfo.Insurer  = args[1]
+packageinfo.Consignee  = args[2]
+packageinfo.Temprature, err = strconv.Atoi(args[3])
+if err != nil {
+    return nil, errors.New("2nd argument must be a numeric string")
+	}
+packageinfo.PackageDes = args[4]
+packageinfo.Owner = args[5]
+packageinfo.PkgStatus = "In-Valid"
+if packageinfo.Temprature < 5 && packageinfo.Temprature > -5 {
+    packageinfo.PkgStatus = "Valid"
+  }
 
 bytes, err := json.Marshal(&packageinfo)
 if err != nil {
         fmt.Println("Could not marshal personal info object", err)
         return nil, err
- }
+  }
 
 err = stub.PutState("1Z20170426", bytes)
 if err != nil {
   return nil, err
-}
+  }
 
 return nil, nil
 }
@@ -87,6 +101,7 @@ if function == "init" {
 } else if function == "deliverpkg"{
   return t.deliverpkg(stub,args)
 }
+
 fmt.Println("invoke did not find func: " + function)
 
 return nil, errors.New("Received unknown function invocation: " + function)
@@ -94,22 +109,34 @@ return nil, errors.New("Received unknown function invocation: " + function)
 
 // create - invoke function to create new asset using given key/value pair
 func (t *SimpleChaincode) create(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-var key string
-var err error
 fmt.Println("running create()")
 
+var key string
+var err error
+
 if len(args) != 6 {
-  return nil, errors.New("Incorrect number of arguments. Expecting 6. name of the key and value to set")
-}
+  return nil, errors.New("Incorrect number of arguments. Expecting 7 in order of PkgID, Shipper, Insurer, Consignee, Temprature, PackageDes, Owner")
+  }
 
 var packageinfo PackageInfo
 
 key = args[0]
+
+packageinfo.PkgId  = args[0]
 packageinfo.Shipper = args[1]
-packageinfo.Consignee  = args[2]
-packageinfo.Temprature = args[3]
-packageinfo.PackageDes = args[4]
-packageinfo.Owner = args[5]
+packageinfo.Insurer = args[2]
+packageinfo.Consignee  = args[3]
+packageinfo.Temprature, err = strconv.Atoi(args[4])
+if err != nil {
+    return nil, errors.New("5th argument must be a numeric string")
+	}
+packageinfo.PackageDes = args[5]
+packageinfo.Owner = args[6]
+
+packageinfo.PkgStatus = "In-Valid"
+if packageinfo.Temprature < 5 && packageinfo.Temprature > -5 {
+    packageinfo.PkgStatus = "Valid"
+  }
 
 bytes, err := json.Marshal(&packageinfo)
 if err != nil {
@@ -133,7 +160,7 @@ var err error
 fmt.Println("running updatetemp()")
 
 if len(args) != 2 {
-  return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+  return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and temprature value to set")
   }
 
 
@@ -149,13 +176,23 @@ if err != nil {
 
 err = json.Unmarshal(valAsbytes, &packageinfo)
 if err != nil {
-      fmt.Println("Could not marshal personal info object", err)
+      fmt.Println("Could not marshal  info object", err)
       return nil, err
   }
+// validate pkd exist or not by checking temprature
+if packageinfo.PkgId != key{
+  return nil, errors.New("Invalid PackageId Passed")
+  }
 
-// check wheather the pkg temprature is in acceptable range
+// check wheather the pkg temprature is in acceptable range and package in in valid status
+if packageinfo.PkgStatus != "Valid" {
+    return nil, errors.New("Temprature thershold crossed - Package in Invalid state")
+  }
 
-packageinfo.Temprature = args[1]
+packageinfo.Temprature, err = strconv.Atoi(args[1])
+if err != nil {
+    return nil, errors.New("2nd argument must be a numeric string")
+	}
 
 bytes, err := json.Marshal(&packageinfo)
 if err != nil {
@@ -174,12 +211,12 @@ return nil, nil
 
 // deliverpkg - update owner og package
 func (t *SimpleChaincode) deliverpkg(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+fmt.Println("running deliverpkg()")
 var key , jsonResp string
 var err error
-fmt.Println("running deliverpkg()")
 
 if len(args) != 2 {
-  return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+  return nil, errors.New("Incorrect number of arguments. Expecting 2. PkgId and New Owner")
   }
 
   key = args[0]
@@ -198,10 +235,16 @@ if len(args) != 2 {
         return nil, err
     }
 
-  // check wheather the pkg temprature is in acceptable range
-  if packageinfo.Temprature > "5" {
-    return nil, errors.New("Temprature thershold crossed - Package in Invalid state")
-  }
+// validate pkd exist or not by checking temprature
+  if packageinfo.PkgId != key{
+    return nil, errors.New("Invalid PackageId Passed")
+    }
+
+  // check wheather the pkg temprature is in acceptable range and package in in valid status
+  if packageinfo.PkgStatus != "Valid" {
+      return nil, errors.New("Temprature thershold crossed - Package in Invalid state")
+    }
+
   packageinfo.Owner = args[1]
 
   bytes, err := json.Marshal(&packageinfo)
@@ -216,8 +259,8 @@ if len(args) != 2 {
     }
 
   return nil, nil
-
 }
+
 
 
 // Query is our entry point for queries
@@ -227,6 +270,8 @@ fmt.Println("query is running " + function)
 // Handle different functions
 if function == "read" { //read a variable
   return t.read(stub, args)
+} else if function == "gethistory"{
+  return t.gethistory(stub,args)
 }
 fmt.Println("query did not find func: " + function)
 
@@ -239,14 +284,14 @@ return nil, errors.New("Received unknown function query: " + function)
 func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 var key, jsonResp string
 var err error
+key = args[0]
 
 if len(args) != 1 {
-  jsonResp = "{\"Error\":\"Incorrect number of arguments. Expecting name of the key to query " + key + "\"}"
+  jsonResp = "{\"Error\":\"Incorrect number of arguments. Expecting PkgID to query " + key + "\"}"
   //return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
   return nil, errors.New(jsonResp)
 }
 
-key = args[0]
 valAsbytes, err := stub.GetState(key)
 if err != nil {
   jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
@@ -260,7 +305,73 @@ if err != nil {
       return nil, err
 }
 
+// validate pkd exist or not by checking temprature
+  if packageinfo.PkgId != key{
+    return nil, errors.New("Invalid PackageId Passed")
+    }
+
+fmt.Println(packageinfo.PkgId)
 fmt.Println(packageinfo.Shipper)
+fmt.Println(packageinfo.Insurer)
+fmt.Println(packageinfo.Consignee)
+fmt.Println(packageinfo.Temprature)
+fmt.Println(packageinfo.PackageDes)
+fmt.Println(packageinfo.Owner)
+fmt.Println(packageinfo.PkgStatus)
+
 return valAsbytes, nil
 //  return packageinfo, nil
+}
+
+// gethistory - query function to read key/value pair
+func (t *SimpleChaincode) gethistory(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+type AuditHistory struct {
+		TxId    string   `json:"txId"`
+		Value   PackageInfo   `json:"value"`
+}
+var history []AuditHistory;
+var packageinfo PackageInfo
+
+var key, jsonResp string
+var err error
+key = args[0]
+
+if len(args) != 1 {
+  jsonResp = "{\"Error\":\"Incorrect number of arguments. Expecting PkgID to query " + key + "\"}"
+  //return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+  return nil, errors.New(jsonResp)
+}
+
+// Get History
+resultsIterator, err := stub.GetHistoryForKey(key)
+if err != nil {
+  return nil, errors.New("Invalid PkgId has been Passed")
+}
+defer resultsIterator.Close()
+
+for resultsIterator.HasNext() {
+  txID, historicValue, err := resultsIterator.Next()
+  if err != nil {
+    return nil, errors.New("gethistory failed")
+  }
+
+  var tx AuditHistory
+  tx.TxId = txID                             //copy transaction id over
+  json.Unmarshal(historicValue, &packageinfo)     //un stringify it aka JSON.parse()
+  if historicValue == nil {                  //marble has been deleted
+    var emptyPkg PackageInfo
+    tx.Value = emptyPkg                 //copy nil marble
+  } else {
+    json.Unmarshal(historicValue, &packageinfo) //un stringify it aka JSON.parse()
+    tx.Value = packageinfo                      //copy marble over
+  }
+  history = append(history, tx)              //add this tx to the list
+}
+fmt.Printf("- getHistoryFor Package returning:\n%s", history)
+
+//change to array of bytes
+historyAsBytes, _ := json.Marshal(history)     //convert to array of bytes
+return historyAsBytes, nil
+
+
 }
